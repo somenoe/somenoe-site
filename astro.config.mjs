27096937e0010737c0ad69cdd1053b91422f7e8f
somenoe <otashi24@gmail.com>
2024-10-28 18:47:35 +0700

    🚀 Re-config domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.somenoe.com/',
+	site: 'https://somenoe.com/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
